d75f4e01522b55f0e22080b4119d1dd41a79c0f0
samuel-bazinet <bazinet.samuel1@gmail.com>
2023-02-11 11:36:46 -0500

    First commit

---
+++ build_start_all_debug.sh
@@ -0,0 +1,5 @@
+cargo build
+./target/debug/server
+./target/debug/human_client
+./target/debug/pet_client
+./target/debug/game_client
---
+++ src/game_client.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello from game client!");
+}
