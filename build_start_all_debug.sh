90906c65d5076e664b5f29ccefee1c7563b111f5
Samuel Bazinet <bazinet.samuel1@gmail.com>
2023-02-18 16:12:27 -0500

    updated script to run server and clients in parallel

--- build_start_all_debug.sh
+++ build_start_all_debug.sh
@@ -1,5 +1,6 @@
 cargo build --release
-./target/release/server
-./target/release/human_client
-./target/release/pet_client
-./target/release/game_client
+./target/release/human_client & 
+./target/release/pet_client &
+./target/release/game_client &
+./target/release/server 
+
