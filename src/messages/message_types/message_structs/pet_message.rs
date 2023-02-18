7431c0f535e6021af4c7683cfa00b3d5214f0c24
Samuel Bazinet <bazinet.samuel1@gmail.com>
2023-02-18 15:49:47 -0500

    Added the pet client

--- src/messages/message_types/message_structs/pet_message.rs
+++ src/messages/message_types/message_structs/pet_message.rs
@@ -7,6 +7,7 @@ use crate::message_traits::serializable::Serializable;
 
 use std::fmt::Display;
 
+/// A message representing a pet that can be serialized
 #[derive(Clone)]
 pub struct PetMessage {
     initial: u8,
@@ -14,7 +15,16 @@ pub struct PetMessage {
     age: u8,
 }
 
+impl PetMessage {
+    /// Generate a pet message by proving the fields
+    pub fn from(initial: u8, class: u8, age: u8) -> Self {
+        PetMessage { initial, class: PetClass::try_from(class).unwrap(), age }
+    }
+}
+
 impl Serializable for PetMessage {
+
+    /// Serialize the message
     fn serialize(&self) -> Vec<u8> {
         let mut return_bytes = vec![MessageId::Pet.to_u8()];
         let initial_bytes = self.initial.to_ne_bytes();
@@ -32,6 +42,7 @@ impl Serializable for PetMessage {
         return_bytes
     }
 
+    /// Deserialize the messages
     fn deserialize(bytes: Vec<u8>) -> Self {
         let initial = bytes[0];
         let class = PetClass::try_from(bytes[1]).unwrap();
--- src/pet_client.rs
+++ src/pet_client.rs
@@ -1,3 +1,24 @@
+use message_enum::{
+    constants, messages::message_types::message_structs::pet_message::PetMessage,
+    messaging_service::sending_client::SendingClient,
+};
+use rand::Rng;
+
+use std::{thread, time};
+
 fn main() {
-    println!("Hello from pet client!");
+    println!("Starting pet client");
+    let mut rng = rand::thread_rng();
+    loop {
+        let message = PetMessage::from(
+            rng.gen_range(65..91),
+            rng.gen_range(0..2),
+            rng.gen(),
+        );
+        let client = SendingClient::from(constants::LOCAL_HOST, constants::HUMAN_PORT);
+        println!("Sending {message} to server");
+        client.send_message_server(message);
+        let sleep_time = time::Duration::from_secs(2);
+        thread::sleep(sleep_time);
+    }
 }
